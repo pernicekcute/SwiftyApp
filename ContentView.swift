c1cb0f5eeb210ec0328b4d8b2b5943d349cefa46
pernicekcute <pernicekcute@gmail.com>
2026-08-23 18:07:59 +0200

    Update ContentView.swift

--- ContentView.swift
+++ ContentView.swift
@@ -28,7 +28,7 @@ struct CustomToolbarButton: View {
         Button {
             showSheet = true
         } label: {
-            Image(systemName: "person.3.fill
+            Image(systemName: "person.3.fill"
 ")
                 .frame(width: 36, height: 36)
         }
