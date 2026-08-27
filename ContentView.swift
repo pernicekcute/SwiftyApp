4c0ca62708eff707250cb60b4e7638e42eb1b8d7
pernicekcute <pernicekcute@gmail.com>
2026-08-27 08:22:23 +0200

    Update ContentView.swift

--- ContentView.swift
+++ ContentView.swift
@@ -4,30 +4,59 @@ struct ContentView: View {
     @State private var selectedTab = 0
 
     var body: some View {
-        VStack(spacing: 20) {
-            Picker("Select Tab", selection: $selectedTab) {
-                Text("Globe").tag(0)
-                Text("Text").tag(1)
-            }
-            .pickerStyle(.segmented)
-            .padding(.horizontal)
-
-            TabView(selection: $selectedTab) {
-                VStack {
-                    Image(systemName: "globe")
-                        .imageScale(.large)
-                        .foregroundStyle(.tint)
-                }
-                .tag(0)
+        TabView(selection: $selectedTab) {
+            VStack(spacing: 20) {
+                Text("Welcome!")
+                    .font(.title2)
+                    .bold()
                 
-                VStack {
-                    Text("Hello, world!")
+                Image(systemName: "globe")
+                    .imageScale(.large)
+                    .foregroundStyle(.tint)
+                
+                Text("Hello, world!")
+            }
+            .tabItem {
+                Label("Home", systemImage: "house.fill")
+            }
+            .tag(0)
+
+            TabBarView()
+                .tabItem {
+                    Label("TabBar", systemImage: "list.dash")
                 }
                 .tag(1)
-            }
-            .tabViewStyle(.page)
         }
-        .padding()
+    }
+}
+
+struct TabBarView: View {
+    @State private var innerSelection = 0
+
+    var body: some View {
+        NavigationStack {
+            VStack {
+                Picker("Options", selection: $innerSelection) {
+                    Text("Overview").tag(0)
+                    Text("Settings").tag(1)
+                }
+                .pickerStyle(.segmented)
+                .padding()
+
+                Spacer()
+
+                if innerSelection == 0 {
+                    Text("Here's the Overview page!")
+                        .font(.headline)
+                } else {
+                        Text("Here's the Settings page!")
+                        .font(.headline)
+                }
+
+                Spacer()
+            }
+            .navigationTitle("TabBar page")
+        }
     }
 }
 
