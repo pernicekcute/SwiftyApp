5758a0120dd959d53cd3ce8d40edf934972e4622
pernicekcute <pernicekcute@gmail.com>
2026-08-30 14:53:47 +0200

    Update SwiftyAppApp.swift

--- SwiftyAppApp.swift
+++ SwiftyAppApp.swift
@@ -2,9 +2,21 @@ import SwiftUI
 
 @main
 struct SwiftyAppApp: App {
+    // Register the AppDelegate to handle orientation locks
+    @UIApplicationDelegateAdaptor(AppDelegate.self) var appDelegate
+
     var body: some Scene {
         WindowGroup {
             ContentView()
         }
+    }
+}
+
+// AppDelegate to globally enforce portrait mode
+class AppDelegate: NSObject, UIApplicationDelegate {
+    static var orientationLock = UIInterfaceOrientationMask.portrait
+
+    func application(_ application: UIApplication, supportedInterfaceOrientationsFor window: UIWindow?) -> UIInterfaceOrientationMask {
+        return AppDelegate.orientationLock
     }
 }
