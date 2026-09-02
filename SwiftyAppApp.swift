37d3a0f540157394f6e8d0ee93af4778e0368128
pernicekcute <pernicekcute@gmail.com>
2026-09-02 21:48:37 +0200

    Update SwiftyAppApp.swift

--- SwiftyAppApp.swift
+++ SwiftyAppApp.swift
@@ -1,24 +1,26 @@
 import SwiftUI
 import UIKit
 
-@main
-struct SwiftyAppApp: App {
-    @AppStorage("useNewStyle") private var useNewStyle: Bool = true
-    
-    init() {
-        // Přečtení nastavení při startu
-        let isNewStyle = UserDefaults.standard.bool(forKey: "useNewStyle")
-        
-        if isNewStyle {
-            // Nový styl: Obnovení výchozího iOS vzhledu
-            UINavigationBar.appearance().standardAppearance = UINavigationBarAppearance()
-            UINavigationBar.appearance().scrollEdgeAppearance = nil
-            UINavigationBar.appearance().compactAppearance = nil
+class ThemeManager {
+    static func applyTheme(useNewStyle: Bool) {
+        if useNewStyle {
+            // Obnovení nového/systémového vzhledu
+            let navAppearance = UINavigationBarAppearance()
+            navAppearance.configureWithDefaultBackground()
             
-            UITabBar.appearance().standardAppearance = UITabBarAppearance()
-            UITabBar.appearance().scrollEdgeAppearance = nil
+            UINavigationBar.appearance().standardAppearance = navAppearance
+            UINavigationBar.appearance().scrollEdgeAppearance = navAppearance
+            UINavigationBar.appearance().compactAppearance = navAppearance
+
+            let tabAppearance = UITabBarAppearance()
+            tabAppearance.configureWithDefaultBackground()
+            
+            UITabBar.appearance().standardAppearance = tabAppearance
+            UITabBar.appearance().scrollEdgeAppearance = tabAppearance
+            
+            UIView.appearance(whenContainedInInstancesOf: [UIAlertController.self]).backgroundColor = nil
         } else {
-            // Starý styl: Neprůhledný vzhled
+            // Starý neprůhledný vzhled
             let navAppearance = UINavigationBarAppearance()
             navAppearance.configureWithOpaqueBackground()
             
@@ -34,6 +36,16 @@ struct SwiftyAppApp: App {
             
             UIView.appearance(whenContainedInInstancesOf: [UIAlertController.self]).backgroundColor = .systemBackground
         }
+    }
+}
+
+@main
+struct SwiftyAppApp: App {
+    @AppStorage("useNewStyle") private var useNewStyle: Bool = true
+
+    init() {
+        let isNewStyle = UserDefaults.standard.bool(forKey: "useNewStyle")
+        ThemeManager.applyTheme(useNewStyle: isNewStyle)
     }
 
     var body: some Scene {
