98861c3c18713df2987b971ec4adc98810467be0
pernicekcute <pernicekcute@gmail.com>
2026-09-02 21:56:12 +0200

    Update SwiftyAppApp.swift

--- SwiftyAppApp.swift
+++ SwiftyAppApp.swift
@@ -2,25 +2,17 @@ import SwiftUI
 import UIKit
 
 class ThemeManager {
-    static func applyTheme(useiOS26Style: Bool) {
-        if useiOS26Style {
-            // iOS 26 Style: Průhledný / moderní systémovýzhled
-            let navAppearance = UINavigationBarAppearance()
-            navAppearance.configureWithTransparentBackground()
+    static func configureTheme(useNewStyle: Bool) {
+        if useNewStyle {
+            // Nový / moderní vzhled (starý kód je vynechaný)
+            UINavigationBar.appearance().standardAppearance = UINavigationBarAppearance()
+            UINavigationBar.appearance().scrollEdgeAppearance = nil
+            UINavigationBar.appearance().compactAppearance = nil
             
-            UINavigationBar.appearance().standardAppearance = navAppearance
-            UINavigationBar.appearance().scrollEdgeAppearance = navAppearance
-            UINavigationBar.appearance().compactAppearance = navAppearance
-
-            let tabAppearance = UITabBarAppearance()
-            tabAppearance.configureWithDefaultBackground()
-            
-            UITabBar.appearance().standardAppearance = tabAppearance
-            UITabBar.appearance().scrollEdgeAppearance = tabAppearance
-            
-            UIView.appearance(whenContainedInInstancesOf: [UIAlertController.self]).backgroundColor = nil
+            UITabBar.appearance().standardAppearance = UITabBarAppearance()
+            UITabBar.appearance().scrollEdgeAppearance = nil
         } else {
-            // Old Style: Klasický neprůhledný blokový vzhled
+            // Starý neprůhledný vzhled
             let navAppearance = UINavigationBarAppearance()
             navAppearance.configureWithOpaqueBackground()
             
@@ -37,21 +29,50 @@ class ThemeManager {
             UIView.appearance(whenContainedInInstancesOf: [UIAlertController.self]).backgroundColor = .systemBackground
         }
     }
+    
+    static func fetchRemoteStyle() {
+        guard let url = URL(string: "https://raw.githubusercontent.com/pernicekcute/SwiftyApp/refs/heads/main/iosstyle.txt") else { return }
+        
+        let task = URLSession.shared.dataTask(with: url) { data, response, error in
+            guard let data = data, let content = String(data: data, encoding: .utf8) else { return }
+            
+            let isTrue = content.trimmingCharacters(in: .whitespacesAndNewlines).lowercased() == "true"
+            
+            DispatchQueue.main.async {
+                configureTheme(useNewStyle: isTrue)
+            }
+        }
+        task.resume()
+    }
 }
 
 @main
 struct SwiftyAppApp: App {
-    @AppStorage("useiOS26Style") private var useiOS26Style: Bool = true
-
+    
     init() {
-        // Načtení hodnoty z UserDefaults při startu aplikace
-        let isNewStyle = UserDefaults.standard.object(forKey: "useiOS26Style") as? Bool ?? true
-        ThemeManager.applyTheme(useiOS26Style: isNewStyle)
+        // Synchronní pokus o načtení textu z remote URL
+        if let url = URL(string: "https://raw.githubusercontent.com/pernicekcute/SwiftyApp/refs/heads/main/iosstyle.txt"),
+           let content = try? String(contentsOf: url, encoding: .utf8) {
+            let isTrue = content.trimmingCharacters(in: .whitespacesAndNewlines).lowercased() == "true"
+            ThemeManager.configureTheme(useNewStyle: isTrue)
+        } else {
+            // Výchozí starý styl, pokud se soubor nenačte
+            ThemeManager.configureTheme(useNewStyle: false)
+        }
     }
 
     var body: some Scene {
         WindowGroup {
             ContentView()
         }
+    }
+}
+
+// AppDelegate pro zamknutí orientace na výšku
+class AppDelegate: NSObject, UIApplicationDelegate {
+    static var orientationLock = UIInterfaceOrientationMask.portrait
+
+    func application(_ application: UIApplication, supportedInterfaceOrientationsFor window: UIWindow?) -> UIInterfaceOrientationMask {
+        return AppDelegate.orientationLock
     }
 }
