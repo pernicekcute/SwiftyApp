ab2e832843fdd34b410c833b7826b0c7377d0b32
pernicekcute <pernicekcute@gmail.com>
2026-09-02 21:33:50 +0200

    Update SwiftyAppApp.swift

--- SwiftyAppApp.swift
+++ SwiftyAppApp.swift
@@ -1,9 +1,26 @@
 import SwiftUI
+import UIKit
 
 @main
 struct SwiftyAppApp: App {
-    // Register the AppDelegate to handle orientation locks
-    @UIApplicationDelegateAdaptor(AppDelegate.self) var appDelegate
+    
+    init() {
+        // 1. Klasický neprůhledný vzhled pro Navigation Bar (horní lišta)
+        let navAppearance = UINavigationBarAppearance()
+        navAppearance.configureWithOpaqueBackground()
+        UINavigationBar.appearance().standardAppearance = navAppearance
+        UINavigationBar.appearance().scrollEdgeAppearance = navAppearance
+        navAppearance.compactAppearance = navAppearance
+
+        // 2. Klasický neprůhledný vzhled pro Tab Bar (dolní lišta)
+        let tabAppearance = UITabBarAppearance()
+        tabAppearance.configureWithOpaqueBackground()
+        UITabBar.appearance().standardAppearance = tabAppearance
+        UITabBar.appearance().scrollEdgeAppearance = tabAppearance
+
+        // 3. Vypnutí průhlednosti a skleněných efektů u systémových oken a prvků
+        UIView.appearance(whenContainedInInstancesOf: [UIAlertController.self]).backgroundColor = .systemBackground
+    }
 
     var body: some Scene {
         WindowGroup {
@@ -11,6 +28,7 @@ struct SwiftyAppApp: App {
         }
     }
 }
+
 
 // AppDelegate to globally enforce portrait mode
 class AppDelegate: NSObject, UIApplicationDelegate {
